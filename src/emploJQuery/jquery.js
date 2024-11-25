a9a177b863c5a599738a698e280a64584ebc7bd6
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2024-11-25 21:04:17 +0100

    Practica de JQuery

---
+++ src/emploJQuery/jquery.js
@@ -0,0 +1,21 @@
+$(() => {
+    console.log("Documento listo");
+    
+    //cambiar valor por defecto del input
+    $("#texto").val("adios");
+
+    //colocar texto del button
+    $("button").html("<i>hola</i>");
+
+    // función para cambiar de texto en el input
+    // $("button").on("click", function() {
+    //     console.log($("#texto").val("Jhonatan"));        
+    // })
+
+    $("button").on({
+        click: function() {
+            $("#texto").val("Jhonatan"); 
+        }click: function(){
+      console.log("hola");
+    }})
+})
