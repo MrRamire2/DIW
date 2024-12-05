b61c5aeea8f5f165663d0a0391d3e74d0c2b770c
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2024-12-05 16:33:21 +0100

    aplicación de login y mejoras

--- src/ud6/actividad-2/js/check-user.js
+++ src/ud6/actividad-2/js/check-user.js
@@ -8,25 +8,33 @@ const error = document.getElementById("error");
 
 submit.addEventListener("click", () => {
     
+    //paso por todos los usuarios y comprueba que exista
     users.forEach(user => {
+
+        //salt
+        let salt = user["password_salt"];
+        //constraseña con salt
+        let password_salt = password.value + salt;
+
+        //contraseña con salt encriptada
+        const hash = CryptoJS.SHA256(password_salt).toString();
+
+        //para comparar
+        console.log(hash);
+        console.log(user["password_hash"]);
     
-        if (email.value === user["email"] && password.value === user["password_hash"]) {
+        //comprueba que exista el usuario con los datos ingresados
+        if (email.value === user["email"] && hash === user["password_hash"]) {
             
             localStorage.setItem("users_log", JSON.stringify(user));
             error.style.visibility = "hidden";
 
+            if (user["is_first_login"] === 1) {
+                document.location.href="../views/change-password.html";
+            }
+
         } else {
             error.style.visibility = "visible";
         }
-
-
-        
-        console.log(user["email"]); 
-        console.log(user["password_hash"]); 
-        console.log(email.value); 
-        console.log(password.value); 
-    
     });
-
 });
-
