41273c5fadbe17af3a508ca177c08ec68d3c7c84
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-16 01:51:50 +0100

    Update admin-options.js

--- ud5/actividad-1/js/admin-options.js
+++ ud5/actividad-1/js/admin-options.js
@@ -44,11 +44,11 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
     } else {
         editNews.addEventListener("click", () => {
-            document.location.href = "../news-edit.html"; // Subir un nivel
+            document.location.href = "./news-edit.html";
         });
     
         editUsers.addEventListener("click", () => {
-            document.location.href = "../users-edit.html"; // Subir un nivel
+            document.location.href = "./users-edit.html";
         });
     }
 });
