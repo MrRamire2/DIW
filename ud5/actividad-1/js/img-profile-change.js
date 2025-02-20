b311fc174c505887f6906778d6a9967750ca73cb
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-20 16:18:32 +0100

    Update img-profile-change.js

--- ud5/actividad-1/js/img-profile-change.js
+++ ud5/actividad-1/js/img-profile-change.js
@@ -7,7 +7,12 @@ $(async function () {
 
     const src_img = userData.data().profile_url;
 
-    const img_default = "../images/users/default.png";
+    if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
+        const img_default = "./images/users/default.png";
+    } else {
+        const img_default = "../images/users/default.png";
+    }
+    
 
     if (src_img == "") {
         $('#img-profile-logout').attr('src', img_default);
