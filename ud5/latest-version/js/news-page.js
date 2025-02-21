9551856e5418c07ff6e7e2c6044a727451b788a9
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-21 22:48:54 +0100

    Cambios

--- ud5/latest-version/js/news-page.js
+++ ud5/latest-version/js/news-page.js
@@ -9,17 +9,29 @@ document.addEventListener("DOMContentLoaded", async function () {
     const title = news.data().title;
     const allContent = news.data().content;
 
-    let article = "";
-    let classImg = "m-auto float-none w-11/12 md:float-left md:w-2/3 md:my-3 md:ml-3 md:mr-6";
-    let classP = "m-3";
+    // let article = "";
+    // let classImg = "m-auto float-none w-11/12 md:float-left md:w-2/3 md:my-3 md:ml-3 md:mr-6";
+    // let classP = "m-3";
 
-    $.each(allContent, (index, row) => {
-        article += "<article>";
-        $.each(row, (index, content) => {
+    let article = "";
+    let classImg = "completo";
+    let classP = "completo";
+
+    $.each(allContent, (i, row) => {
+        article += "<article class='articulo'>";
+        $.each(row, (j, content) => {
             if (content.type === 'paragraph') {
-                article += `<p class="${classP}">${content.content}</p>`;
+                if (j > 0) {
+                    article += `<p class="medio">${content.content}</p>`;
+                } else {
+                    article += `<p class="${classP}">${content.content}</p>`;
+                }
             } else {
-                article += `<img class="${classImg}" src="${content.src}" alt="Image">`; 
+                if (j > 0) {
+                    article += `<img class="medio" src="${content.src}" alt="Image">`; 
+                } else {
+                    article += `<img class="${classImg}" src="${content.src}" alt="Image">`; 
+                }
             }
         });
         article += "</article>";
@@ -29,4 +41,10 @@ document.addEventListener("DOMContentLoaded", async function () {
     $("#title").html(title);
 
     $("#container").html(article);
-})
+
+    $(".articulo").each(function() {
+        if ($(this).find(".medio").length > 0) {
+            $(this).children().first().addClass("medio");
+        }
+    });
+});
