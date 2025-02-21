5a485a836a21879c85e0427d8858b455f0dd790b
Jhonatan Andres Ramirez <j.ramirezap@gmail.com>
2025-02-21 18:13:42 +0100

    Update news-page.js

--- ud5/latest-version/js/news-page.js
+++ ud5/latest-version/js/news-page.js
@@ -10,22 +10,21 @@ document.addEventListener("DOMContentLoaded", async function () {
     const allContent = news.data().content;
 
     let article = "";
-
+    let classImg = "m-auto float-none w-11/12 md:float-left md:w-2/3 md:my-3 md:ml-3 md:mr-6";
+    let classP = "m-3";
 
     $.each(allContent, (index, row) => {
         article += "<article>";
         $.each(row, (index, content) => {
             if (content.type === 'paragraph') {
-                article += `<p>${content.content}</p>`;
+                article += `<p class="${classP}">${content.content}</p>`;
             } else {
-                article += `<img src="${content.src}" alt="Image">`; 
+                article += `<img class="${classImg}" src="${content.src}" alt="Image">`; 
             }
         });
         article += "</article>";
     
     })
-
-    //!!!!!!!!!!!!!!HACER LOS ESTILOS!!!!!!!!!!!!!!!!
 
     $("#title").html(title);
 
